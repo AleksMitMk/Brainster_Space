626fc39a62fc5fb49c7575c909ad5eeb4afd6e5e
Aleksandar Mitev <57228808+AleksMitMk@users.noreply.github.com>
2020-12-25 15:20:24 +0100

    changing routes to the menu links

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ function App() {
           <NavbarBr />
           <Switch>
             <Route exact path="/" component={HomePage}/>
-            <Route exact path="https://aleksmitmk.github.io/Brainster_Space/" component={HomePage}/>
-            <Route path="/home" component={HomePage}/>
+            <Route path="https://aleksmitmk.github.io/Brainster_Space/home" component={HomePage}/>
             <Route path="/EventSpace" component={EventsSpace}/>
             <Route path="/Academies" component={Academies}/>
             <Route path="/Nastani" component={MyCalendar} />
